1170e6c8e8173ef4f893a8c29b9f24574aacfeb3
kj-duggu <harshvardhan152k@gmail.com>
2022-02-02 22:53:07 +0530

    First Commit

---
+++ demoproject/src/demoproject/test.java
@@ -0,0 +1,14 @@
+package demoproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey kalpesh");
+		int a=10; int b=50; int c;
+		c = a+b;
+		System.out.println("Addition Of Two Number:-"+c);
+
+	}
+
+}
